ef5ebd42f0846914a57e739417938c3054f5225a
MiXyz <72651636+mixyzteam12@users.noreply.github.com>
2021-11-18 07:00:38 +0700

    Update kernel_build.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -17,10 +17,17 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
+cd ~
 git clone --depth=1 $KERNEL_SOURCE $KERNEL_BRANCH $DEVICE_CODENAME
-mkdir clang
-wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r433403.tar.gz -O "clang-r433403.tar.gz"
-tar -xf clang-r433403.tar.gz -C clang
+  mkdir ~/clang-llvm
+  mkdir ~/gcc64-aosp
+  mkdir ~/gcc32-aosp
+  ! [[ -f ~/clang-r383902b1.tar.gz ]] && wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-qpr3-release/clang-r383902b1.tar.gz -P ~/
+  tar -C ~/clang-llvm/ -zxvf ~/clang-r383902b1.tar.gz
+  ! [[ -f ~/android-11.0.0_r35.tar.gz ]] && wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-11.0.0_r35.tar.gz -P ~/
+  tar -C ~/gcc64-aosp/ -zxvf ~/android-11.0.0_r35.tar.gz
+  ! [[ -f ~/android-11.0.0_r34.tar.gz ]] && wget http://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-11.0.0_r34.tar.gz -P ~/
+  tar -C ~/gcc32-aosp/ -zxvf ~/android-11.0.0_r34.tar.gz
 # clang set as Clang Default
 
 # Main Declaration
@@ -73,15 +80,13 @@ tg_post_msg "<b>xKernelCompiler:</b><code>Compilation has started</code>"
 cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
-    CC=${CLANG_ROOTDIR}/bin/clang \
-    AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-    NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-    OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
-    OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
-    STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
-    LD=${CLANG_ROOTDIR}/bin/ld.lld \
-    CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+    CC=clang \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    LD=ld.lld \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
